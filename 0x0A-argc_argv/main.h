cbb084b2981831bcdf81d9a0b3cc6ae8b17e05ca
mohammedhassbo <mhassbo004@gmail.com>
2023-08-09 06:34:54 -0700

    main.h

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,14 +1,15 @@
-#ifndef MAIN_H
-		
-#define MAIN_H
-		
+#include "main.h"
 
-		
-int _putchar(char c);
-		
-int _atoi(char *s);
-		
+/**
+* main - prints the name of the program 
+* @argc: number of arguments
+* @argv: array of arguments
+*
+* Return: Always 0 (Success)
+*/
 
-		
-#endif
-
+int main(int argc _attribute_((unused)), char "argv[ ])
+{
+ printf("%s\n, *argv);
+ return (0);
+}
